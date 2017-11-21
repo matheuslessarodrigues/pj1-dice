7babd57d2491657aa4dd8179ddb4561c22ae5902
Matheus Lessa <matheuslessarodrigues@gmail.com>
2017-11-21 20:42:23 -0200

    pronto

---
+++ Assets/Scripts/PlayParticleOnCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayParticleOnCollision : MonoBehaviour
+{
+	public ParticleSystem[] particles;
+
+	private void OnCollisionEnter( Collision collision )
+	{
+		foreach( var particle in particles )
+		{
+			particle.Play();
+		}
+	}
+}
---
+++ Assets/Scripts/Roll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Roll : MonoBehaviour
+{
+	public Rigidbody diceRigidbody;
+
+	public float impulse = 10.0f;
+	public Vector3 minTorque = new Vector3( 0.0f, 0.0f, 0.0f );
+	public Vector3 maxTorque = new Vector3( 10.0f, 10.0f, 0.0f );
+
+	private void OnMouseDown()
+	{
+		Vector3 torque = new Vector3();
+		torque.x = Random.Range( minTorque.x, maxTorque.x );
+		torque.y = Random.Range( minTorque.y, maxTorque.y );
+		torque.z = Random.Range( minTorque.z, maxTorque.z );
+
+		diceRigidbody.AddTorque( torque, ForceMode.Impulse );
+		diceRigidbody.AddForce( new Vector3( 0.0f, impulse, 0.0f ), ForceMode.Impulse );
+	}
+}
